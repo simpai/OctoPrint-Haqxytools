358a9f1b01e63674d281c67d7b512d611df64071
simpai <junghun.cha@gmail.com>
2019-02-26 14:00:10 +0900

    learning

--- octoprint_haqxytools/__init__.py
+++ octoprint_haqxytools/__init__.py
@@ -11,65 +11,8 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
-class HaqxytoolsPlugin(octoprint.plugin.SettingsPlugin,
-                       octoprint.plugin.AssetPlugin,
-                       octoprint.plugin.TemplatePlugin):
+class HaqxytoolsPlugin(octoprint.plugin.StartupPlugin):
+    def on_after_startup(self):
+        self._logger.info("Hello World!")
 
-	def on_after_startup(self):
-	    self._logger.info("Hello HaqXY!")
-
-	##~~ SettingsPlugin mixin
-
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
-
-	##~~ AssetPlugin mixin
-
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/haqxytools.js"],
-			css=["css/haqxytools.css"],
-			less=["less/haqxytools.less"]
-		)
-
-	##~~ Softwareupdate hook
-
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
-		return dict(
-			haqxytools=dict(
-				displayName="Haqxytools Plugin",
-				displayVersion=self._plugin_version,
-
-				# version check: github repository
-				type="github_release",
-				user="simpai",
-				repo="OctoPrint-Haqxytools",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/simpai/OctoPrint-Haqxytools/archive/{target_version}.zip"
-			)
-		)
-
-
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Haqxytools Plugin"
-
-def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = HaqxytoolsPlugin()
-
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
-
+__plugin_implementation__ = HaqxytoolsPlugin()
